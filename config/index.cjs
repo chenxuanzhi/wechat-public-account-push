d49ebf71ff5ff1ce423d1a2b276d658d79739d04
chenxuanzhi <44158644+chenxuanzhi@users.noreply.github.com>
2022-11-29 00:21:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxb5375cfab4a8f099',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'ca285776a3165d9ebc9c45927c2f0ea1',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oHl3m6K91TZzCivtwm4xV8sw2PcQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
